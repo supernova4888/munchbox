0da16612574cd5aab6ee4ed1fa9ef60f210632f1
Rabunnisa Zabiulla <rabia.zabiulla@gmail.com>
2021-04-26 12:07:16 +0200

    initial commit

---
+++ src/main/java/com/se/munchbox/recipe/MunchBoxApplication.java
@@ -0,0 +1,28 @@
+package com.se.munchbox.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class MunchBoxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MunchBoxApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+
+            }
+        };
+    }
+}
---
+++ src/main/java/com/se/munchbox/recipe/RecipeRepository.java
@@ -0,0 +1,4 @@
+package com.se.munchbox.recipe;
+
+public class RecipeRepository {
+}
