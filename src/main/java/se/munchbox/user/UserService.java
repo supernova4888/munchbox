a31dcc1baea7b958acce26fb3cc08de8bb6dbd39
radhad-ux <70577224+radhad-ux@users.noreply.github.com>
2021-04-26 16:11:28 +0200

    updated UserService Class

--- src/main/java/se/munchbox/user/UserService.java
+++ src/main/java/se/munchbox/user/UserService.java
@@ -5,7 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
-@Service()
+@Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
