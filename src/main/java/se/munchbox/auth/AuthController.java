10ead3caf53893c9be24bd776f458eeca9176559
sailathatammana <sailathatammana1997@gmail.com>
2021-04-26 14:52:19 +0200

    Implemented the functionality

--- src/main/java/se/munchbox/auth/AuthController.java
+++ src/main/java/se/munchbox/auth/AuthController.java
@@ -1,4 +1,42 @@
 package se.munchbox.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import se.munchbox.user.User;
+import se.munchbox.user.UserService;
+
+@RestController
 public class AuthController {
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody User user) {
+        userService.register(user);
+
+        String token = authService.createAuthToken(user.getEmail());
+        AuthResponse authResponse = new AuthResponse(token);
+
+        return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> authenticate(@RequestBody AuthRequest authRequest) {
+        try {
+            String token = authService.authenticate(authRequest.getEmail(), authRequest.getPassword());
+            AuthResponse authResponse = new AuthResponse(token);
+
+            return new ResponseEntity<>(authResponse, HttpStatus.OK);
+        } catch (AuthenticationException authenticationException) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
 }
