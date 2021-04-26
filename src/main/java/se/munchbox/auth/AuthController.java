d27f22eb672ba7ebded714fc6fce0783135f1105
sailathatammana <sailathatammana1997@gmail.com>
2021-04-26 14:57:11 +0200

    Implemented the functionality

--- src/main/java/se/munchbox/auth/AuthController.java
+++ src/main/java/se/munchbox/auth/AuthController.java
@@ -7,8 +7,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import se.munchbox.user.User;
-import se.munchbox.user.UserService;
 
 @RestController
 public class AuthController {
