393b8da0d0da054fc89d49dcf0ac3438228096be
radhad-ux <70577224+radhad-ux@users.noreply.github.com>
2021-04-27 17:13:28 +0200

    Implemented UserPreference Class

--- src/main/java/se/munchbox/userPref/UserPreferenceRepository.java
+++ src/main/java/se/munchbox/userPref/UserPreferenceRepository.java
@@ -1,4 +1,11 @@
 package se.munchbox.userPref;
 
-public class UserPreferenceRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserPreferenceRepository extends JpaRepository<UserPreference, Long> {
+    List<UserPreference> findAllByUserId(Long userId);
 }
