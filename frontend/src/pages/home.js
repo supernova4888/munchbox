d38b0e52f23646b5bb45e3bab485ff07ff730c9f
Fiona <thompsonfiona@outlook.com>
2021-04-28 19:29:25 +0200

    Add logo resources, remove React images

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+import logo from "../resources/logo.png";
+
 export default function HomePage() {
 return (
     <div>
-        HomePageee
+        <h1>HomePage</h1>
+        You are on the home page now. Below, you should see a large logo for testing.
+        <img src={logo}/>
     </div>
 )
 }
