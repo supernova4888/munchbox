3dc72d44c3934e9a36f8dbd82a97da36819aff2e
Fiona <thompsonfiona@outlook.com>
2021-04-29 14:04:08 +0200

    Inital commit navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <footer className="navbarFooter">
             <ul>
-                <li><Link to="/home">Home</Link></li>
+                <li><Link to="/home">Home </Link></li>
                 <li><Link to="/profile">Profile</Link></li>
                 <li><Link to="/post">Post</Link></li>
             </ul>
