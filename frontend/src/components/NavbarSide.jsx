c95abadf468c7e95c5ec7e5bcf82a41d91ad56a7
Fiona <thompsonfiona@outlook.com>
2021-04-29 16:29:14 +0200

    Navbar link encompass icon and text

--- frontend/src/components/NavbarSide.jsx
+++ frontend/src/components/NavbarSide.jsx
@@ -14,36 +14,44 @@ export default function NavbarSide() {
                 </div>
             </div>
             <div className="navItem">
-                <div className="navIcon">
-                    <img className="svgIcon" src={home}/>
-                </div>
-                <div className="navLabel">
-                    <Link to="/home">Home </Link>
-                </div>
+                <Link to="/home">
+                    <div className="navIcon">
+                        <img className="svgIcon" src={home}/>
+                    </div>
+                    <div className="navLabel">
+                        Home
+                    </div>
+                </Link>
             </div>
             <div className="navItem">
-                <div className="navIcon">
-                    <img className="svgIcon" src={profile}/>
-                </div>
-                <div className="navLabel">
-                    <Link to="/profile">Profile</Link>
-                </div>
+                <Link to="/profile">
+                    <div className="navIcon">
+                        <img className="svgIcon" src={profile}/>
+                    </div>
+                    <div className="navLabel">
+                        Profile
+                    </div>
+                </Link>
             </div>
             <div className="navItem">
-                <div className="navIcon">
-                    <img className="svgIcon" src={pot}/>
-                </div>
-                <div className="navLabel">
-                    <Link to="/post">Post</Link>
-                </div>
+                <Link to="/post">
+                    <div className="navIcon">
+                        <img className="svgIcon" src={pot}/>
+                    </div>
+                    <div className="navLabel">
+                        Post
+                    </div>
+                </Link>
             </div>
             <div className="navItem">
-                <div className="navIcon">
-                    <img className="svgIcon" src={spinner}/>
-                </div>
-                <div className="navLabel">
-                    <Link to="/post">Spinner</Link>
-                </div>
+                <Link to="/post">
+                    <div className="navIcon">
+                        <img className="svgIcon" src={spinner}/>
+                    </div>
+                    <div className="navLabel">
+                        Spinner
+                    </div>
+                </Link>
             </div>
         </div>
     );
