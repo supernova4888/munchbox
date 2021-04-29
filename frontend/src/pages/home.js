b6b9e36c322e50437bf3ed332b5af11314abeaa9
Fiona <thompsonfiona@outlook.com>
2021-04-29 12:39:51 +0200

    Style recipe card, refactor to RecipeCardSmall

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import StarRating from "../components/StarRating";
 import logo from "../resources/logo.png";
+import RecipeCardSmall from "../components/RecipeCardSmall";
 
 export default function HomePage() {
 return (
@@ -9,7 +10,9 @@ return (
         <h1>HomePage</h1>
         <p>You are on the home page now. Below you'll see a start rating for testing.</p>
         <StarRating />
-
+        <RecipeCardSmall />
+        <RecipeCardSmall />
+            <RecipeCardSmall />
     </div>
 )
 }
