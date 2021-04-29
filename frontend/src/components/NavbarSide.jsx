fc0d95a0b18f351ef834597e4f9396a439b071dc
Fiona <thompsonfiona@outlook.com>
2021-04-29 16:05:18 +0200

    Basic layout and imports for side navigation bar

---
+++ frontend/src/components/NavbarSide.jsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import pot from "../resources/pan.svg";
+import home from "../resources/home.svg";
+import spinner from "../resources/pinwheel.svg";
+import profile from "../resources/profile-user.svg";
+import squarelogo from "../resources/logo_transparent_square.png";
+
+export default function NavbarSide() {
+    return (
+        <div className="navbarSide">
+            <div className="navItem">
+                <div className="navIcon">
+                    <img className="navLogo" src={squarelogo}/>
+                </div>
+            </div>
+            <div className="navItem">
+                <div className="navIcon">
+                    <img className="svgIcon" src={home}/>
+                </div>
+                <div className="navLabel">
+                    <Link to="/home">Home </Link>
+                </div>
+            </div>
+            <div className="navItem">
+                <div className="navIcon">
+                    <img className="svgIcon" src={profile}/>
+                </div>
+                <div className="navLabel">
+                    <Link to="/profile">Profile</Link>
+                </div>
+            </div>
+            <div className="navItem">
+                <div className="navIcon">
+                    <img className="svgIcon" src={pot}/>
+                </div>
+                <div className="navLabel">
+                    <Link to="/post">Post</Link>
+                </div>
+            </div>
+            <div className="navItem">
+                <div className="navIcon">
+                    <img className="svgIcon" src={spinner}/>
+                </div>
+                <div className="navLabel">
+                    <Link to="/post">Spinner</Link>
+                </div>
+            </div>
+        </div>
+    );
+}
