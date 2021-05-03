b07eb24457a373ceefd26d6bc64e8056e44553a2
Fiona <thompsonfiona@outlook.com>
2021-05-03 16:05:26 +0200

    Add back arrow with link to login and register page

--- frontend/src/pages/loginpage.jsx
+++ frontend/src/pages/loginpage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import LoginForm from "./auth/LoginForm";
 
 export default function LoginPage() {
     return (
         <div>
+            <Link to="/mobile"><h1><i className="fas fa-arrow-circle-left"></i></h1></Link>
         <LoginForm />
         </div>
     )
--- frontend/src/pages/registerpage.jsx
+++ frontend/src/pages/registerpage.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import RegisterForm from "./auth/RegisterForm";
 
 export default function RegisterPage() {
     return (
         <div>
+            <Link to="/mobile">
+            <h1><i className="fas fa-arrow-circle-left"></i></h1></Link>
         <RegisterForm />
         </div>
     )
