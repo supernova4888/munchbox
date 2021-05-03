c9f5f881e35372c205aa7187403d0a78eb4135f6
Rabunnisa Zabiulla <rabia.zabiulla@gmail.com>
2021-05-03 15:08:05 +0200

    Router is added for review page in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import PostPage from "./pages/post.jsx";
 import ProfilePage from "./pages/profile.jsx";
 import SpinnerPage from "./pages/spinner.jsx";
 import Navbar from "./components/Navbar.jsx";
+import ReviewPage from "./pages/review";
 
 
 export default function App() {
@@ -21,6 +22,7 @@ export default function App() {
             <BrowserRouter>
                 <Switch>
                     <Route exact component={HomePage} path="/home" />
+                    <Route exact component={ReviewPage} path="/review" />
                     <Route exact component={PostPage} path="/post" />
                     <Route exact component={ProfilePage} path="/profile" />
                     <Route exact component={SpinnerPage} path="/spinner" />
