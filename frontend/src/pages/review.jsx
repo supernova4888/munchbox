91bef7a32aa4b2925ef0a70b4bb418509e1c0125
Rabunnisa Zabiulla <rabia.zabiulla@gmail.com>
2021-05-03 17:24:54 +0200

    All the fields are added in review.jsx

--- frontend/src/pages/review.jsx
+++
@@ -1,6 +0,0 @@
-import react from "react";
-export default function ReviewPage(){
-return(
-    <div>This is a review page </div>
-)
-}
---
+++ frontend/src/pages/review/review.jsx
@@ -0,0 +1,26 @@
+import react from "react";
+
+import recipeImageGeneral from "../resources/recipe-03.jpg"
+import RecipeCardLarge from "../components/RecipeCardLarge";
+export default function ReviewPage(){
+return(
+    <div>
+        <h3>This is a review page</h3>
+        <h6>Posted by </h6>
+        <RecipeCardLarge/>
+        <h6>Chicken Pizza</h6>
+        <div>My RATING</div>
+        <div>COMMENTS:</div>
+        <Form>
+
+        </Form>
+
+
+
+
+
+
+         </div>
+
+)
+}
