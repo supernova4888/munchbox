23097d0e295ba0e08a5910001d02dc7f6e951ad5
Rabunnisa Zabiulla <rabia.zabiulla@gmail.com>
2021-05-03 14:05:03 +0200

    ReviewApi is implemented with all the required methods

--- frontend/src/api/ReviewApi.js
+++ frontend/src/api/ReviewApi.js
@@ -1,1 +1,16 @@
 import Api from "./Api";
+class ReviewApi{
+    createReview(id,reviews){
+        return Api.post('/posts/'+id+'/reviews',reviews)
+    }
+    getAllReviews(id){
+        return Api.get('/posts/'+id+'/reviews');
+    }
+    updateReview(id,reviews){
+        return Api.put('/posts/'+id+'/reviews',reviews);
+
+    }
+    deleteReview(id){
+        return Api.delete('reviews/'+id);
+    }
+}
