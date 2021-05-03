bfe36bb7e497824f3f4112a9222704efd147ed2d
Fiona <thompsonfiona@outlook.com>
2021-05-03 20:52:41 +0200

    Style logout button

--- frontend/src/components/LogoutButton.jsx
+++ frontend/src/components/LogoutButton.jsx
@@ -3,12 +3,14 @@ import exit from "../resources/navbar/exit-white.png"
 export default function LogoutButton({ onLogout }) {
     return (
         <div className="logout">
+            <div className="exitBox">
             <button
                 className="logoutButton"
                 onClick={onLogout}
             >
-                <img className="svgIconSide" src={exit} />
+                <img className="svgIconExit" src={exit} />
             </button>
+            </div>
         </div>
     );
 }
