1999db96aec248eaf714271dc942292f9e8c036f
supernova4888 <58419750+supernova4888@users.noreply.github.com>
2021-05-04 11:51:05 +0200

    created state to save image

--- frontend/src/pages/post.jsx
+++ frontend/src/pages/post.jsx
@@ -1,20 +1,33 @@
 import React from "react";
+import Axios from "axios";
+import { useState } from "react";
 
 // link from the button to here
 
 export default function PostPage() {
 
-    const uploadImage = (files) => {
-        console.log(files[0]);
+    const [imageSelected, setImageSelected] = useState("");
 
-    }
+    const uploadImage = () => {
+        // constructs the formData
+        const formData = new FormData()
+        formData.append("file", imageSelected)
+        // upload presets - public (connects to cloudinary)
+        formData.append("upload_preset", "blp4p8lu")
+        
+        // post request
+        Axios.post("https://api.cloudinary.com/v1_1/dt0zgbuyg/image/upload", formData).then((response) => {console.log(response);
+        });
+    };
 
 
     return (
         <div>
             <h1>PostPage - Add a new recipe</h1>
             <p>You are on the post page now.</p>
-            <input type="file" onChange={(event) => {uploadImage(event.target.files)}}/>
+            <input type="file" onChange={(event) => setImageSelected(event.target.files[0])}/>
+
+            <button onClick={uploadImage}> Upload Image</button>
 
         </div>
     )
