638c1591fd8f56daa19f8599703ba230bb1752d9
Fiona <thompsonfiona@outlook.com>
2021-05-04 19:00:09 +0200

    Refactor _recipe.css to match RecipeCardMedium, add new RecipeCardLarge

--- frontend/src/components/RecipeCardLarge.jsx
+++ frontend/src/components/RecipeCardLarge.jsx
@@ -2,17 +2,16 @@ import React from "react";
 import recipeImageGeneral from "../resources/recipe-03.jpg"
 import sampleProfile from "../resources/profilepic200px-05.jpg";
 
-export default function RecipeCardMedium({ recipePost }) {
+export default function RecipeCardLarge({ recipePost }) {
     return (
-        <div className="recipeCardMedium">
+        <div className="recipeCardLarge">
             <div className="recipeImageBox">
-                <img className="recipeImageMedium" src={recipeImageGeneral}/>
+                <img className="recipeImageLarge" src={recipeImageGeneral}/>
             </div>
             <div className="recipeTitleBox">{recipePost.title}</div>
             <div className="recipePostedBy">
                 <img className="userProfilePicSmall" src={sampleProfile}/>
                 <div className="userNameSmall">{recipePost.userName}</div>
-
             </div>
         </div>
     );
