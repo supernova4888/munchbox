d3ab70aadffefcd1f63fafe5396a8275b773d7d8
fnt90 <thompsonfiona@outlook.com>
2021-05-04 23:08:17 +0200

    Fix missing HomePage info from previous merge

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,21 +1,28 @@
 import React, { useState, useEffect } from "react";
-
-import StarRating from "../components/StarRating";
-import logo from "../resources/logo.png";
-import RecipeCardSmall from "../components/RecipeCardSmall";
-import RecipeCardLarge from "../components/RecipeCardLarge";
+import RecipeCardMedium from "../components/RecipeCardMedium";
+import RecipePostApi from "../api/RecipePostApi";
 import Form from "../components/Form";
-import { useState, useEffect } from "react";
 
 export default function HomePage() {
+        //Const
+        const [recipes, setRecipes] = useState([]);
+        useEffect(() => {
+                RecipePostApi.getAllRecipes()
+                    .then(({data}) => setRecipes(data))
+                    .catch((err) => console.error(err));
+        }, [setRecipes]);
 
+        const RecipesArray = recipes.map((recipePost) => (
+            <RecipeCardMedium key={recipePost.id} recipePost={recipePost}/>
+
+        ));
 return (
-    <div>
-        <h1>HomePage</h1> 
-        <p>You are on the home page now. Below you'll see a sample recipes.</p>
+    <div className="pageBody">
+            <h1>HomePage</h1>
+            <p>You are on the home page now. Below you'll see all recipes, with a sample image and sample user profile image.</p>
 
         <Form />
-        
+
         <div className="recipeContainer">
             {RecipesArray}
         </div>
