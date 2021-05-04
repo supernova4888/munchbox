42f7de4933f1b664d5b1743e44fb0e057b7c0675
Fiona <thompsonfiona@outlook.com>
2021-05-04 14:02:59 +0200

    Begin recipePost API to fetch to homepage, edit RecipeCardLarge

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,26 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import StarRating from "../components/StarRating";
-import logo from "../resources/logo.png";
+import RecipePostApi from "../api/RecipePostApi";
 import RecipeCardSmall from "../components/RecipeCardSmall";
 import RecipeCardLarge from "../components/RecipeCardLarge";
 
 
 export default function HomePage() {
+    //Const
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        RecipePostApi.getAllRecipes()
+            .then(({data}) => setRecipes(data))
+            .catch((err) => console.error(err));
+    }, [setRecipes]);
+
+    const RecipesArray = recipes.map((recipePost) => (
+        <RecipeCardLarge key={recipePost.id} recipePost={recipePost}/>
+
+));
+
+console.log(recipes)
+
 return (
     <div>
         <h1>HomePage</h1>
         <p>You are on the home page now. Below you'll see a sample recipes.</p>
 
         <div className="recipeContainer">
-            <RecipeCardLarge/>
-            <RecipeCardLarge/>
-            <RecipeCardLarge/>
-            <RecipeCardLarge/>
-            <RecipeCardSmall />
-            <RecipeCardSmall />
-            <RecipeCardSmall />
-            <RecipeCardSmall />
+            {RecipesArray}
+
         </div>
     </div>
 )
