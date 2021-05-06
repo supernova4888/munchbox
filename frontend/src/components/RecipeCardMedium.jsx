b867daf932bc9d03b00b56157db80ddd3595b3c4
supernova4888 <58419750+supernova4888@users.noreply.github.com>
2021-05-06 12:10:08 +0200

    added link to rcipeCardMedium, TBT

--- frontend/src/components/RecipeCardMedium.jsx
+++ frontend/src/components/RecipeCardMedium.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import recipeImageGeneral from "../resources/recipe-03.jpg"
 import sampleProfile from "../resources/profilepic200px-05.jpg";
+import { Link } from "react-router-dom";
 
 export default function RecipeCardMedium({ recipePost }) {
     return (
         <div className="recipeCardMedium">
+
             <div className="recipeImageBox">
-                <img className="recipeImageMedium" src={recipeImageGeneral}/>
+                <Link to={`post/${recipePost.id}`}>
+                    <img 
+                    className="recipeImageMedium" 
+                    src={recipeImageGeneral}
+                    />
+                </Link>
             </div>
+
             <div className="recipeTitleBox">{recipePost.title}</div>
             <div className="recipePostedBy">
                 <img className="userProfilePicSmall" src={sampleProfile}/>
