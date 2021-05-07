7836cf6a06e8d34662c30f05f553af93722d261e
supernova4888 <58419750+supernova4888@users.noreply.github.com>
2021-05-07 15:54:02 +0200

    finished the img functions

--- frontend/src/components/UploadRecipeImg.jsx
+++ frontend/src/components/UploadRecipeImg.jsx
@@ -49,53 +49,11 @@ export default function UploadRecipeImg({uploadImg}) {
 
     useEffect(()=> {
           postToCloude();
-       //to fire postToCloude after the imgFile changed
-    },[imgFile]);
+       //to fire postToCloude after the imgURL changed
+    },[imgURL]);
 
-Axios.post("https://api.cloudinary.com/v1_1/dt0zgbuyg/image/upload", formData).then((response) => {setImageURL(response.data.url)
-        });
-
-
-    const [imgFile,setImgFile] = useState(null);
-
-    const imgUpload= e => {
-        console.log(“fuckthat”,e);
-
-        let imageFile = e[0]; //from console it index 0
-
-        let imagePreset = new FormData();
-        imagePreset.append(“file”, imageFile);
-        imagePreset.append(“upload_preset”, “HikingBuddy”)
-        console.log(“what the fuck”,imagePreset);
-        setImgFile(imagePreset);
-    }
-    const postToCloude = async()=> {
-        try {
-            if (imgFile!== null) {
-              const response = await axios.post(
-                //Cloud link to upload the image to
-                “https://api.cloudinary.com/v1_1/dz2vr4bag/image/upload”,
-                imgFile
-              );
-              //await axios will send a response
-              console.log(“Responce:“,response.data.secure_url);
-              //simulate the event object so its doesn’t clash with handleChange
-              uploadImg({
-                target: {
-                  name: “imageUrl”,
-                  value: response.data.secure_url,
-                },
-              });
-            }
-          } catch (error) {
-            console.log(error);
-          }
-    }
-    useEffect(()=> {
-          postToCloude();
-       //to fire postToCloude after the imgFile changed
-    },[imgFile]);
-
+// Axios.post("https://api.cloudinary.com/v1_1/dt0zgbuyg/image/upload", formData).then((response) => {setImageURL(response.data.url)
+//         });
 
     return (
         <div>
