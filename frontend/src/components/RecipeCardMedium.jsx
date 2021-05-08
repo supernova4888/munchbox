f79fbe18982a004f7f440eb8237eef8a80d188f4
supernova4888 <58419750+supernova4888@users.noreply.github.com>
2021-05-08 19:52:34 +0200

    testing different ways to retrieve img from cloud

--- frontend/src/components/RecipeCardMedium.jsx
+++ frontend/src/components/RecipeCardMedium.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import recipeImageGeneral from "../resources/recipe-03.jpg"
 import sampleProfile from "../resources/profilepic200px-05.jpg";
+import { Image } from "cloudinary-react";
 
 export default function RecipeCardMedium({ recipePost }) {
     return (
         <div className="recipeCardMedium">
             <div className="recipeImageBox">
                 {/* <img className="recipeImageMedium" src={recipeImageGeneral}/> */}
-                <img className="recipeImageMedium" src={recipePost.imgURL}/>
+                {/* // this is working. So I need to make sure that recipePost.imgURL constains the correct image cloud link */}
+                <Image className="recipeImageMedium" cloudName="dt0zgbuyg" publicId="https://res.cloudinary.com/dt0zgbuyg/image/upload/v1620495963/munchbox/fqdvd3pn1y3kclwuzmks.png"/>
             </div>
             <div className="recipeTitleBox">{recipePost.title}</div>
             <div className="recipePostedBy">
