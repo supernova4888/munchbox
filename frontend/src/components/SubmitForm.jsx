6b2b989040f151674ff37d9e4d6427a23bc70df4
supernova4888 <58419750+supernova4888@users.noreply.github.com>
2021-05-08 20:59:44 +0200

    refactored SubmitForm to include all new post form fields

--- frontend/src/components/SubmitForm.jsx
+++ frontend/src/components/SubmitForm.jsx
@@ -1,90 +1,55 @@
 import React from 'react'
-import Axios from "axios";
 import { useState, useEffect } from "react";
-import { Image } from "cloudinary-react";
-import ReactImageUploadComponent from "react-images-upload";
-import { useRecoilState, selector } from "recoil";
-// State
-import { recipeImgState } from "../pages/post";
 
+export default function SubmitForm({cloudImgURL}) {
 
-export default function UploadRecipeImg() {
+    const [recipes, setRecipes] = useState([]);
+    const [title, setTitle] = useState("");
+    const [link, setLink] = useState("");
+    // TODO: add dropdown menu to choose pre-defined ingredient
+    const [ingredient, setIngredient] = useState ("");
 
-    // Global state
-    const [imageURL, setImageURL] = useState("");
-    const [imageSelected, setImageSelected] = useState("");
+    useEffect(() => {
+        RecipePostApi.getAllRecipes()
+        .then((response) => setRecipes(response.json))
+        .catch((err) => console.log(err));
+        }, []);
 
-    const uploadImage = (e) => {
-        // constructs the formData
-        // const imageFile = e[0];
-        const formData = new FormData()
-        formData.append("file", imageSelected)
-        // upload presets - public (connects to cloudinary)
-        formData.append("upload_preset", "blp4p8lu")
-        // post request
-        console.log("before setImageURL", formData);
-        setImageURL(formData);
-        console.log("after imgURL");
-    };
-
-    useEffect(()=> {
-          postToCloud();
-       //to fire postToCloude after the image has been uploaded/changed
-    },[imageURL]);
-
-    const postToCloud = async() => {
+    async function createRecipe (event, cloudImgURL) {
+        event.preventDefault();
+        console.log("inside createRecipe function");
+        const newRecipe = {
+            title: title,
+            body: link,
+            imgURL: {cloudImgURL},
+            mainIngredient: ingredient
+        }
+        console.log(newRecipe);
         try {
-            if (imageURL !== null) {
-              const response = await Axios.post("https://api.cloudinary.com/v1_1/dt0zgbuyg/image/upload", imageURL);
-              //await axios will send a response
-              console.log("received response from cloud");
-              console.log(response.data.url);
-              //simulate the event object so its doesn’t clash with handleChange
-              // TODO: understand this one?
-            //   uploadImg({
-            //     target: {
-            //       name: 'imageURL',
-            //       value: response.data.url,
-            //     },
-            //   });
-            }
-          } catch (error) {
-            console.log(error);
-          }
+            const response = await RecipePostApi.createRecipe(newRecipe);
+            console.log(response.data);
+            setRecipes(response.data);
+        } catch (e) {
+            console.error(e);
+        }
     }
-    // need selector to be able to use in PostPage
-    // const imgCloudURL = selector({
-    //     key: "imgCloudURL",
-    //     get: ({get}) => {
-    //         const imageURL = get(recipeImgState)
-
-    //         return imageURL
-    //     }
-    // })
-
-// Axios.post("https://api.cloudinary.com/v1_1/dt0zgbuyg/image/upload", formData).then((response) => {setImageURL(response.data.url)
-//         });
 
     return (
         <div>
-            <h3>Add recipe image</h3>
+            <h3>Add recipe details</h3>
 
-                {/* <ReactImageUploadComponent
-            singleImage={true}
-            onChange={imageURL}/>}
-            buttonText= 'Choose images'
-            withLabel={false}
-            withIcon={false}
-            imgExtension={['.jpg', '.gif', '.png', '.gif']}
-            buttonClassName='' /> */}
-{/* 
-            <input type="file" onChange={(e) => uploadImage(e)}/>
+            <form className="recipeForm" onSubmit={createRecipe}>
 
-            <button className="buttonUpload" onClick={uploadImage}> Upload Image</button> */}
+                <input className="form-control" placeholder="Enter Recipe Title" type="text" onChange={(e) => setTitle(e.target.value)}/>
 
+                <input className="form-control" placeholder="Paste Link Here" type="text" onChange={(e) => setLink(e.target.value)}/>
 
-                <input type="file" onChange={(e) => setImageSelected(e.target.files[0])}/>
-                <button className="buttonUpload" onClick={uploadImage}> Upload Image</button>
+                <h3>Select recipe main ingredient:</h3>
+                <input className="form-control" type="text" onChange={(e) => setIngredient(e.target.value)}/>
+
+                <button className="buttonRegister" type="submit">Submit</button>
+
+            </form>
         </div>
     )
 }
