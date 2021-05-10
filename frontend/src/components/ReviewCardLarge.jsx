c95686f180544ecbbb413fc6d12fe35fb0ebc65c
supernova4888 <58419750+supernova4888@users.noreply.github.com>
2021-05-10 11:06:18 +0200

    removed Image-cloudinary from ReviewCardLarge

--- frontend/src/components/ReviewCardLarge.jsx
+++ frontend/src/components/ReviewCardLarge.jsx
@@ -1,10 +1,8 @@
 import recipeImageGeneral from "../resources/recipe-03.jpg"
 import sampleProfile from "../resources/profilepic200px-05.jpg";
-import { Image } from "cloudinary-react";
-
 export default function(){
     return(<div className="recipeImageBox">
-        <Image className="recipeImageLarge" cloudName="dt0zgbuyg" publicId={recipePost.imgURL}/>
+        <img className="recipeImageLarge" src={recipeImageGeneral}/>
             <div className="recipeTitleBox">Chicken Pizza</div>
     </div>
     );
