a8b538ce6cc06e16c31f5e76e00e27ae037be254
supernova4888 <58419750+supernova4888@users.noreply.github.com>
2021-05-10 11:03:30 +0200

    added Image-cloudinary to RecipeCardLarge component

--- frontend/src/components/ReviewCardLarge.jsx
+++ frontend/src/components/ReviewCardLarge.jsx
@@ -1,8 +1,10 @@
 import recipeImageGeneral from "../resources/recipe-03.jpg"
 import sampleProfile from "../resources/profilepic200px-05.jpg";
+import { Image } from "cloudinary-react";
+
 export default function(){
     return(<div className="recipeImageBox">
-        <img className="recipeImageLarge" src={recipeImageGeneral}/>
+        <Image className="recipeImageLarge" cloudName="dt0zgbuyg" publicId={recipePost.imgURL}/>
             <div className="recipeTitleBox">Chicken Pizza</div>
     </div>
     );
