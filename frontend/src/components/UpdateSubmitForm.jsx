24ffda00d44d00e447c6ab229010c61afdfb20c3
supernova4888 <58419750+supernova4888@users.noreply.github.com>
2021-05-11 11:06:05 +0200

    added first structure of page and component

--- frontend/src/components/UpdateSubmitForm.jsx
+++ frontend/src/components/UpdateSubmitForm.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import Popup from "reactjs-popup";
+import { useForm } from "react-hook-form";
 
-export default function UpdateSubmitForm() {
+export default function UpdateSubmitForm({preloadedValues}) {
+
+    const {register, handleSubmit} = useForm();
+
+    const onSubmit = (data) => {
+        alert(JSON.stringify(data));
+    }
+
+    
+
+
     return (
         <div>
+             <h3>Add recipe details </h3>
+
+            <form className="updateRecipeForm" onSubmit={handleSubmit(onSubmit)}>
+
+                <input className="form-control" ref={register} onChange={(e) => setTitle(e.target.value)}/>
+
+                <input className="form-control" ref={register} onChange={(e) => setLink(e.target.value)}/>
+
+                <h3>Select recipe main ingredient:</h3>
+
+                <select id = "dropdown" onChange={(e) => setIngredient(e.target.value)}>
+                    <option>Select main ingredient:</option>
+                    <option value="Beef">Beef</option>
+                    <option value="Veal">Veal</option>
+                    <option value="Pork">Pork</option>
+                    <option value="Lamb and Mutton">Lamb and Mutton</option>
+                    <option value="Poultry">Poultry</option>
+                    <option value="Seafood">Seafood</option>
+                    <option value="Vegetarian">Vegetarian</option>
+                    <option value="Vegan">Vegan</option>
+                </select>
+
+                <Popup trigger={
+                <button className="buttonPost"type="submit"> Update Post </button>} modal nested>
+                    {close => (
+                        <div className="modal">
+                        <button className="close" onClick={close}> &times;  
+                        </button>
+                        <div className="content">
+                        {' '}
+                        🎉 Recipe has been sucessfully updated! 🎉
+                        </div>
+                        </div>)}
+                </Popup>
+    
             
         </div>
     )
--- frontend/src/pages/RecipeUpdatePage.jsx
+++ frontend/src/pages/RecipeUpdatePage.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import UpdateSubmitForm from "../components/UpdateSubmitForm";
+import { useEffect } from "react";
+
 
 export default function RecipeUpdatePage() {
+    const preloadedValues = {
+        title: title,
+        body: link,
+        imgURL: cloudURL,
+        mainIngredient: ingredient
+    }
+
+    const [data , setData] = useState(null)
+    
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setData(await.get)
+        }
+        return () => {
+            cleanup
+        }
+    }, [input])
+
     return (
         <div>
             
         </div>
     )
 }
+
+
+preloadedValues
