4a7c4facb7d969802ae3339c5a71c7fa57428057
supernova4888 <58419750+supernova4888@users.noreply.github.com>
2021-05-11 22:44:49 +0200

    experiment with the RecipeUpdatePage

--- frontend/src/pages/RecipeUpdatePage.jsx
+++ frontend/src/pages/RecipeUpdatePage.jsx
@@ -4,27 +4,59 @@ import { useEffect, useState } from "react";
 import RecipePostApi from "../api/RecipePostApi";
 import Updatebutton from "../resources/images/UpdateButton.png";
 import { Link, useParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
 
 export default function RecipeUpdatePage() {
 
-const [data , setData] = useState(null)
+const [recipes, setRecipes] = useState("");
 const {id} = useParams();
     
-    // Miss: getting the id from the user clicking from the ReviewPage (icon)
+    // TODO: fix icon linking from review to here
+
+    // recipe data is here, but i can't retrieve it
+    // ? why sometimes the useEffect loads and sometimes not?
+    const preloadedValues = {
+        title: "lala"
+       
+    }
+
+    //  body: recipes.,
+    //         imgURL: cloudURL,
+    //         mainIngredient: ingredien
+
+    const {register, handleSubmit} = useForm({
+        defaultValues: preloadedValues
+    });
+
+    const onSubmit = (recipes) => {
+        alert(JSON.stringify(recipes));
+    };
 
     useEffect(() => {
-        const fetchData = async () => {
-            setData(await RecipePostApi.getRecipeById(id))
-        }
-        fetchData()
-    }, [])
-
+            RecipePostApi.getRecipeById(id)
+                .then(({data}) => setRecipes(data))
+                .catch((err) => console.error(err));
+                console.log(recipes);
+        }, [setRecipes]);
 
     return (
         <div>
-        <p>you are in Recipe Update page</p>
+        <p>You are in Recipe Update page</p>
+        {/* broken */}
         {/* <UpdateSubmitForm preloadedValues={data} /> */}
         {/* button to edit img */}
+
+        <form className="updateRecipeForm" onSubmit={handleSubmit(onSubmit)}>
+
+                {/* create display img, pass data to an img component. Then allow for change/new img upload in that image component */}
+
+
+                <input {...register('title')} type="text" />
+                <input {...register('recipeurl')} type="text" />
+                <input {...register('imgURL')} type="text" />
+        </form>
+
         </div>
     )
 }
