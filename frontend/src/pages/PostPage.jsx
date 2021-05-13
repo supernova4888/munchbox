b555194fde8134632c090fff3e634139e6a63f53
supernova4888 <58419750+supernova4888@users.noreply.github.com>
2021-05-13 10:35:30 +0200

    cleaned comments from PostPage

--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Axios from "axios";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Image } from "cloudinary-react";
 import SubmitForm from "../components/SubmitForm";
 import ImageUploader from "../components/ImageUploader";
@@ -10,48 +9,12 @@ export default function PostPage() {
 const [imageURL, setImageURL] = useState("");
 const [imageSelected, setImageSelected] = useState("");
 
-// const uploadImage = (e) => {
-//         e.preventDefault();
-//         // constructs the formData
-//         // const imageFile = e[0];
-//         const formData = new FormData()
-//         console.log(imageSelected);
-//         formData.append("file", imageSelected)
-//         // upload presets - public (connects to cloudinary)
-//         formData.append("upload_preset", "blp4p8lu")
-//         console.log("before setImageURL", formData);
-//         setImageURL(formData);
-//         console.log("after imgURL");
-//     };
-
-//     useEffect(()=> {
-//         postToCloud();
-//        //to fire postToCloud after the image has been uploaded uploadimg()
-//     },[imageURL]);
-
-//     const postToCloud = async() => {
-//         try {
-//             if (imageURL !== null) {
-//                 const response = await Axios.post("https://api.cloudinary.com/v1_1/dt0zgbuyg/image/upload", imageURL);
-//                 setImageURL(response.data.url);
-//                 //await axios will send a response
-//                 console.log("received response from cloud");
-//                 console.log(response.data.url);
-//                 console.log(imageURL);
-//             }
-//         } catch (error) {
-//             console.log(error);
-//         }
-//     }
-
     return (
         <div className="pageBody">
             <h1>Add a new recipe</h1>
 
             <div className="card">
 
-                {/* <input type="file" onChange={(e) => setImageSelected(e.target.files[0])}/>
-                <button className="buttonUpload" onClick={uploadImage}> Upload Image</button> */}
                 <ImageUploader setImage={setImageURL} />
             </div>
             {imageURL !== "" &&
